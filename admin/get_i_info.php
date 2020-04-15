76abb313be9b563ea3e10bcf0dcc258a93ecdb67
Mohamed Yousif <mmbusif@gmail.com>
2020-04-15 12:52:59 +0200

    adding new requirements to issues

--- admin/get_i_info.php
+++ admin/get_i_info.php
@@ -15,7 +15,7 @@ $name = $row1['name'];
 $id =mysqli_real_escape_string($db,$_GET['id']);
 mysqli_query($db,"set names utf8");
 
-$q = "SELECT * FROM `issues` WHERE quarantine_id=$id order by id desc limit 0,10  ;";
+$q = "SELECT * FROM `issues` WHERE quarantine_id=$id and is_done = true order by id desc limit 0,10  ;";
 $result =mysqli_query($db,$q);
 echo '<div class="accordion" id="accordionExample">';
 while($row = mysqli_fetch_assoc($result)){
@@ -25,7 +25,8 @@ $des = $row['issue_description'];
 $img =$row['img'];
 $made_by = $row['made_by'];
 $id = $row['id'];
-
+$is_done = $row['is_done'];
+  $due_date = $row['due_date'];
 echo '<div class="card">
     <div class="card-header" id="heading'.$id.'">
       <h5 class="mb-0">
@@ -38,8 +39,18 @@ echo '<div class="card">
     <div id="collapse'.$id.'" class="collapse" aria-labelledby="heading'.$id.'e" data-parent="#accordionExample">
       <div class="card-body">
         '.$des.'<img src="../'.$img.'" width="auto" height="auto" style="max-width:100%;" />
+        <br><br>
+        
+        <p>Made by: '.$made_by.'</p>
+        <p>Due date: '.$due_date. '</p>
+        <p>Status: ' . $is_done . '</p>
       </div>
     </div>
+
+        <button class="btn btn-link" type="button">
+        Mark as complete
+        </button>
+
   </div>';
 
 
--- admin/hc_list.php
+++ admin/hc_list.php
@@ -404,7 +404,7 @@ xhr.send();
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Information about Qurantine</h5>
+        <h5 class="modal-title" id="exampleModalLabel">Issue log</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
