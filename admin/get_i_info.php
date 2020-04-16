d1a9951c7d3caf65ff4da510a30ac5566892e911
Mohamed Yousif <mmbusif@gmail.com>
2020-04-16 07:05:52 +0200

    fixing view directly the issue

--- admin/get_i_info.php
+++ admin/get_i_info.php
@@ -4,30 +4,31 @@
 require_once('../config.php');
 include('./session2.php');
 
-function getmanager_name($db,$code){
-$q = "SELECT * FROM `users` WHERE id='$code' ";
-$result1 =mysqli_query($db,$q);
-$row1 = mysqli_fetch_assoc($result1);
-$name = $row1['name'];
+function getmanager_name($db, $code)
+{
+  $q = "SELECT * FROM `users` WHERE id='$code' ";
+  $result1 = mysqli_query($db, $q);
+  $row1 = mysqli_fetch_assoc($result1);
+  $name = $row1['name'];
   return $name;
 };
 
-$id =mysqli_real_escape_string($db,$_GET['id']);
-mysqli_query($db,"set names utf8");
+$id = mysqli_real_escape_string($db, $_GET['id']);
+mysqli_query($db, "set names utf8");
 
-$q = "SELECT * FROM `issues` WHERE quarantine_id=$id and is_done = false order by id desc limit 0,10  ;";
-$result =mysqli_query($db,$q);
+$q = "SELECT * FROM `issues` WHERE quarantine_id=$id  order by id desc limit 0,10  ;";
+$result = mysqli_query($db, $q);
 echo '<div class="accordion" id="accordionExample">';
-while($row = mysqli_fetch_assoc($result)){
-$date = $row['date'];
-$time =$row['time'];
-$des = $row['issue_description'];
-$img =$row['img'];
-$made_by = $row['made_by'];
-$id = $row['id'];
-$is_done = $row['is_done'];
-$due_date = $row['due_date'];
-echo '<div class="modal" tabindex="-1" role="dialog">
+while ($row = mysqli_fetch_assoc($result)) {
+  $date = $row['date'];
+  $time = $row['time'];
+  $des = $row['issue_description'];
+  $img = $row['img'];
+  $made_by = $row['made_by'];
+  $id = $row['id'];
+  $is_done = $row['is_done'];
+  $due_date = $row['due_date'];
+  echo '<div class="modal" tabindex="-1" role="dialog">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
 
@@ -59,15 +60,57 @@ echo '<div class="modal" tabindex="-1" role="dialog">
   </div>
 </div>';
 
+echo '
 
+<table class="table table-sm">
+  <thead>
+    <tr>
+      <th scope="col">Field</th>
+      <th scope="col">Description</th>
+
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">ID</th>
+      <td>'.$id.'</td>
+
+    </tr>
+    <tr>
+      <th scope="row">Made by</th>
+      <td>'.$made_by.'</td>
+    </tr>
+        <tr>
+      <th scope="row">Description</th>
+      <td>' . $des . '</td>
+    </tr>
+    <tr>
+      <th scope="row">Date</th>
+      <td>'.$date.'</td>
+    </tr>
+        <tr>
+      <th scope="row">Time</th>
+      <td>'.$time.'</td>
+    </tr>
+    <tr>
+      <th scope="row">image urls</th>
+      <td>'.$img. '</td>
+    </tr>
+        <tr>
+      <th scope="row">Is done</th>
+      <td>' . $is_done . '</td>
+    </tr>
+  </tbody>
+</table>
+                <button class="btn btn-primary btn-sm" type="button" onclick="updateissue(' . $id . ')">
+        Mark as complete
+        </button>
+        <hr><br>
+
+';
 }
 
 
 ?>
 
 </div>
-
-
-
-
-
